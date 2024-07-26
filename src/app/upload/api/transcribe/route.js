18d12fdcbdf2f296f857ea01a6025854d72e273e
adelhassan1 <tomasander12@gmail.com>
2024-07-26 18:52:05 +0300

    redeployment

--- src/app/upload/api/transcribe/route.js
+++ src/app/upload/api/transcribe/route.js
@@ -18,7 +18,7 @@ async function getTranscriptionFile(filename) {
 	const client = new S3Client({
 		region: 'eu-north-1',
 		credentials: {
-			accessKeyId: process.env.AWS_ACCESS_KEY,
+			accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 		}
 	});
@@ -53,7 +53,7 @@ export async function GET(req) {
 	const transcribeClient = new TranscribeClient({
 		region: 'eu-north-1',
 		credentials: {
-			accessKeyId: process.env.AWS_ACCESS_KEY,
+			accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 		}
 	});
